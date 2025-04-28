6ef00788de91ae93a588d920d693282b3a3600fc
Lucas Lopes <lucast96@protonmail.com>
2025-04-28 11:13:30 -0300

    fix: Update broken dependencies paths

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -19,7 +19,7 @@ export default function App(){
         <a href="https://github.com/lukewood-1/top-memory-game" target='_blank' rel='noopener noreferrer'>
           <img className='link_github' src='https://cdn.simpleicons.org/github'  alt="Lukewood's gitHub" />
         </a>
-        <img src='./assets/logo_artistic.png' className='logo' alt="logo" />
+        <img src='/logo_artistic.png' className='logo' alt="logo" />
         <Settings difficulty={difficulty} suit={suit} onDifficultyChange={handleDifficulty} onSuitChange={handleSuit} />
       </header>
       <main>
